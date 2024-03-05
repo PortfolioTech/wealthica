3455569540447ee5fbd06284bc3b67408ed00b89
RenBelDev <renebelleisle@hotmail.com>
2024-03-05 10:42:46 -0500

    +

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,21 @@ function getStoreDevValue() {
 function removeStoreDevValue() {
     localStorage.removeItem("dev");
 }
+
+function isInProduction() {
+    return getNodeEnv() === "production";
+}
+
+function getNodeEnv() {
+    fetch('/node-env')
+        .then(response => response.json())
+        .then(data => {
+            console.log('NODE_ENV:', data.nodeEnv);
+            // Now you can use data.nodeEnv in your client-side code
+        });
+    /*
+    return app.get('/', (req, res) => {
+        res.render('index', { nodeEnv: process.env.NODE_ENV });
+    });*/
+    return "dev";
+}
